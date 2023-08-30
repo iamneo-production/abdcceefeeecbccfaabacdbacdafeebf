fb7b633d65ab04215e187c46ad4eb277c0175be7
Iamneo <techadmin@iamneo.ai>
2023-08-30 11:47:02 +0000

    Backup Commit

--- Cucumber/src/test/java/stepDefination/stepDef.java
+++ Cucumber/src/test/java/stepDefination/stepDef.java
@@ -1,31 +1,66 @@
+package stepdefinitions;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.*;
 
 public class FeedbackStepDefinitions {
 
+    private WebDriver driver;
+
     @Given("the user navigates to JavaTpoint.com")
     public void navigateToJavaTpoint() {
-        // Add code to navigate to the website
+        // Set the path to your ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver();
+
+        // Navigate to JavaTpoint.com
+        driver.get("https://www.javatpoint.com/");
     }
 
     @When("the user clicks on Feedback, then the Feedback page opens")
     public void clickOnFeedback() {
-        // Add code to simulate clicking on Feedback
+        // Simulate clicking on Feedback link
+        WebElement feedbackLink = driver.findElement(By.linkText("Feedback"));
+        feedbackLink.click();
     }
 
     @When("the user submits feedback message")
     public void submitFeedback() {
-        // Add code to simulate submitting feedback
+        // Simulate submitting feedback form
+        WebElement feedbackText = driver.findElement(By.id("feedback-text"));
+        feedbackText.sendKeys("This is a test feedback message.");
+
+        WebElement submitButton = driver.findElement(By.id("submit-button"));
+        submitButton.click();
     }
 
     @Then("feedback should be received on the admin page")
     public void verifyFeedbackReceived() {
-        // Add code to verify feedback received on admin page
+        // Simulate verifying feedback on admin page
+        WebElement feedbackMessage = driver.findElement(By.id("admin-feedback"));
+        assertNotNull(feedbackMessage);
     }
 
     @Then("admin can reply to the user")
     public void adminRepliesToUser() {
-        // Add code to simulate admin replying to the user
+        // Simulate admin replying to the user
+        WebElement replyBox = driver.findElement(By.id("admin-reply"));
+        replyBox.sendKeys("Thank you for your feedback!");
+        
+        WebElement sendButton = driver.findElement(By.id("send-reply"));
+        sendButton.click();
+    }
+    
+    @After
+    public void tearDown() {
+        // Close the browser after each scenario
+        driver.quit();
     }
 }
